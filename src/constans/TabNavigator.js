6a4478b5d7837a88b91f1581d8d77103d20164f4
MuhammadSaad781 <ch.saad781@gmail.com>
2023-02-20 00:54:22 +0500

    Navigation resolved

--- src/constans/TabNavigator.js
+++ src/constans/TabNavigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import PickupRequest from "../screens/PickupRequest";
